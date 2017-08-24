b35f3ada72881160d0a5bf2372d438c242a4f064
Sun Davey <ssunkim7@gmail.com>
2017-08-24 21:50:37 +0000

    added comment model & starting Restful routing

--- app.js
+++ app.js
@@ -1,68 +1,38 @@
 var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose");
+    mongoose    = require("mongoose"),
+    Campground  = require("./models/campground"),
+    seedDB      = require("./seeds");
 
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/final_camping_site", {useMongoClient: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
-
-//Setup Schema
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
-
-/*Campground.create(
-    {
-        name: "Alta Lake",
-        image: "https://s-media-cache-ak0.pinimg.com/originals/40/d8/7d/40d87d82dc61f8b076ce38f887550975.jpg"
-        
-    }, function(err, campground){
-        if(err){
-            console.log(err);
-        }else{
-            console.log("Newly Created Campground: ");
-            console.log(campground);
-        }
-    });*/
-    
- var campgrounds = [
-        {name: "Alta Lake", image: "https://s-media-cache-ak0.pinimg.com/originals/40/d8/7d/40d87d82dc61f8b076ce38f887550975.jpg"},
-        {name: "Curlew Lake", image: "https://media-cdn.tripadvisor.com/media/photo-s/0c/86/18/01/black-beach-resort-is.jpg"},
-        {name: "Fort Flagler", image: "http://www.campinginthenorthwest.com/1000_750_csupload_47164730.jpg?u=1151210587"},
-        {name: "Alta Lake", image: "https://s-media-cache-ak0.pinimg.com/originals/40/d8/7d/40d87d82dc61f8b076ce38f887550975.jpg"},
-        {name: "Curlew Lake", image: "https://media-cdn.tripadvisor.com/media/photo-s/0c/86/18/01/black-beach-resort-is.jpg"},
-        {name: "Fort Flagler", image: "http://www.campinginthenorthwest.com/1000_750_csupload_47164730.jpg?u=1151210587"},
-        {name: "Alta Lake", image: "https://s-media-cache-ak0.pinimg.com/originals/40/d8/7d/40d87d82dc61f8b076ce38f887550975.jpg"},
-        {name: "Curlew Lake", image: "https://media-cdn.tripadvisor.com/media/photo-s/0c/86/18/01/black-beach-resort-is.jpg"},
-        {name: "Fort Flagler", image: "http://www.campinginthenorthwest.com/1000_750_csupload_47164730.jpg?u=1151210587"}
-    ];
+seedDB();
     
 app.get("/", function(req, res){
     res.render("landing");
 });
 
-
+//INDEX -show all the campground
 app.get("/campgrounds", function(req, res){
     Campground.find({}, function(err, allCampgrounds){
         if(err){
             console.log(err);
         }else{
-            res.render("campgrounds", {campgrounds: allCampgrounds});
+            res.render("index", {campgrounds: allCampgrounds});
         }
     });
 });
 
+//CREATE- add new campground to db
 app.post("/campgrounds", function(req, res){
     //get data from form and add to campgrounds array
     var name = req.body.name;
-    var image =req.body.image;
-    var newCampground = {name: name, image: image};
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc};
     //create new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
@@ -73,10 +43,22 @@ app.post("/campgrounds", function(req, res){
     });
 });
 
+//NEW -show from to creat new campground
 app.get("/campgrounds/new", function(req, res){
     res.render("new");
 });
 
+//SHOW -show particular item more info
+app.get("/campgrounds/:id", function(req, res){
+    //find the campground with provided ID
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("show", {campground: foundCampground});
+        }
+    });
+});
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server started!");
 });
---
+++ seeds.js
@@ -0,0 +1,60 @@
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var Comment = require("./models/comment");
+
+var data = [
+    {
+        name: "Alta Lake",
+        image: "https://s-media-cache-ak0.pinimg.com/originals/40/d8/7d/40d87d82dc61f8b076ce38f887550975.jpg",
+        description: "Alta Lake State Park is a publicly owned recreation area located 2 miles southwest of Pateros, Washington, at the northern end of 220-acre Alta Lake, in the mountainous northwest interior of the state."
+    },
+    {   
+        name: "Curlew Lake", 
+        image: "https://media-cdn.tripadvisor.com/media/photo-s/0c/86/18/01/black-beach-resort-is.jpg",
+        description: "Curlew Lake State Park is a 123-acre Washington state park located on Curlew Lake in Ferry County. It is not far from a public access paleontology site and an airfield."
+    },
+    {
+        name: "Fort Flagler", 
+        image: "http://www.campinginthenorthwest.com/1000_750_csupload_47164730.jpg?u=1151210587",
+        description: "Fort Flagler State Park is a Washington state park on the site of Fort Flagler, a former United States Army fort at the northern end of Marrowstone Island. Fort Flagler was a Coast Artillery fort. It was established in 1897 and activated on in 1899."
+    }
+];
+
+
+
+function seedDB(){
+    //remove all the campgrounds
+        Campground.remove({}, function(err){
+        if(err){
+            console.log(err);
+        }
+        console.log("removed campgrounds!");
+        
+         //add a few campgrounds
+        data.forEach(function(seed){
+            Campground.create(seed, function(err, campground){
+                if(err){
+                    console.log(err);
+                }else{
+                    console.log("added a campground");
+                    //create a comment
+                    Comment.create({
+                        text: "This place is great, but I wish there was a toilet",
+                        author: "Atom"
+                    }, function(err, comment){
+                        if(err){
+                            console.log(err);
+                        }
+                        campground.comments.push(comment);
+                        campground.save();
+                        console.log("Created new comment");
+                    });
+                }
+            });
+        });
+    });
+   
+    
+}
+
+module.exports = seedDB;
