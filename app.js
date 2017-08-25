d5321aad03e2b6eb704e10c62b942277020da94e
Sun Davey <ssunkim7@gmail.com>
2017-08-25 17:38:20 +0000

    added login page and handling the login logic

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ app.post("/campgrounds/:id/comments", function(req, res){
     //redirect campground show page
 });
 
-//Auth Routes
+//AUTH ROUTES
 app.get("/register", function(req, res){
     res.render("register");
 });
@@ -132,6 +132,20 @@ app.post("/register", function(req, res){
             res.redirect("/campgrounds");
         });
     });
+});
+
+
+//show login form
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+//handling login logic
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), function(req, res){
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
