e15736dcfb9743336d3823ac6f80bfd9a6ff3667
Sun Davey <ssunkim7@gmail.com>
2017-08-25 17:10:26 +0000

    passport configuration

--- app.js
+++ app.js
@@ -15,6 +15,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
+
+//Passport Configuration
+app.use(require("express-session")({
+    secret: "Life is Wonderful!",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializedUser(User.deserializedUser());
     
 app.get("/", function(req, res){
     res.render("landing");
