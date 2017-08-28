ecd8a55cbd1b812a4432492bbdcf45e0de124b68
Sun Davey <ssunkim7@gmail.com>
2017-08-28 18:33:44 +0000

    Worked on campground Authorization

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -54,13 +54,9 @@ router.get("/:id", function(req, res){
 });
 
 //EDIT campground route
-router.get("/:id/edit", function(req, res){
-    Campground.findById(req.params.id, function(err, foundCampground){
-        if(err){
-            res.redirect("/campgrounds");
-        }else{
+router.get("/:id/edit", checkCampgroundOwnership, function(req, res){
+         Campground.findById(req.params.id, function(err, foundCampground){
             res.render("campgrounds/edit", {campground:foundCampground});
-        }
     });
 });
 
@@ -77,7 +73,7 @@ router.put("/:id", function(req, res){
 });
 
 //DESTROY campground route
-router.delete("/:id", function(req, res){
+router.delete("/:id", checkCampgroundOwnership, function(req, res){
    Campground.findByIdAndRemove(req.params.id, function(err){
        if(err){
            res.redirect("/campgrounds");
@@ -93,6 +89,25 @@ function isLoggedIn(req, res, next){
         return next();
     }
     res.redirect("/login");
+}
+
+function checkCampgroundOwnership(req, res, next){
+     if(req.isAuthenticated()){
+         Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            res.redirect("back");
+        }else{
+            if(foundCampground.author.id.equals(req.user._id)){
+                next();
+            }else{
+                res.redirect("back");
+            }
+            
+        }
+    });
+    }else{
+        res.redirect("back");
+    }
 }
 
 module.exports = router;
