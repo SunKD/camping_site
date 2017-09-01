83f1f8de201d882bc78990f4a6ecd2dc79400d49
Sun Davey <ssunkim7@gmail.com>
2017-09-01 18:31:34 +0000

    fixed bugs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
+    flash       = require("connect-flash"),
     passport    = require("passport"),
     LocalStrategy = require("passport-local"),
     methodOverride = require("method-override"),
@@ -11,9 +12,9 @@ var express     = require("express"),
     seedDB      = require("./seeds");
     
 //Requiring Routes
-var commnetRoutes   = require("./routes/comments"),
-    campgroundRoute = require("./routes/campgrounds"),
-    indexRoutes     = require("./routes/index");
+var commentRoutes    = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes      = require("./routes/index");
 
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/final_camping_site", {useMongoClient: true});
@@ -21,6 +22,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
+app.use(flash());
 //seedDB(); //seed the database
 
 //Passport Configuration
@@ -38,12 +40,14 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
     res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
     next();
 });
 
 app.use("/", indexRoutes);
-app.use("/campgrounds", campgroundRoute);
-app.use("/campgrounds/:id/comments", commnetRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server started!");
